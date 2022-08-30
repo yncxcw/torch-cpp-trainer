32292210c664ab1fe9eadbe3bc259b4df59dd4c1
Wei Chen <weich@nvidia.com>
2022-08-29 23:09:51 -0600

    add tests

---
+++ tests/test_module_utils.cpp
@@ -0,0 +1,20 @@
+// Copyright ynjassionchen@gmail.com
+// #include "../../include/module_utils.hpp"
+
+#include <gtest/gtest.h>
+#include <vector>
+
+TEST(TestCovNxN, TestOutputShape) {
+  // auto conv3x3 = torch::nn::convNxN<3>(3, 20, 1);
+  // auto output_tensor = conv3x3(torch::ones({10, 3, 10, 10}));
+  // auto tensor_shape = output_tensor.sizes();
+  // auto expect_shape = std::vector<int64_t>{10, 3, 10, 10};
+  // EXPECT_EQ(tensor_shape, expect_shape);
+
+  EXPECT_EQ(10, 10);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
