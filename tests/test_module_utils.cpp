5f3ede8e88da8dcf1508be3f04ed12260f81612c
Wei Chen <weich@nvidia.com>
2022-09-04 21:19:47 -0600

    add resnet

--- tests/test_module_utils.cpp
+++ tests/test_module_utils.cpp
@@ -2,14 +2,29 @@
 #include "../include/module_utils.hpp"
 
 #include <gtest/gtest.h>
-#include <iostream>
 #include <vector>
 
-TEST(TestCovNxN, TestOutputShape) {
+TEST(TestCov3x3, TestOutputShape) {
   auto conv3x3 = torch::nn::convNxN<3>(3, 20, 1);
   auto output_tensor = conv3x3(torch::ones({10, 3, 10, 10}));
   auto tensor_shape = output_tensor.sizes();
   auto expect_shape = std::vector<int64_t>{10, 20, 10, 10};
+  EXPECT_EQ(tensor_shape, expect_shape);
+}
+
+TEST(TestCov1x1, TestOutputShape) {
+  auto conv3x3 = torch::nn::convNxN<1>(3, 20, 1);
+  auto output_tensor = conv3x3(torch::ones({10, 3, 10, 10}));
+  auto tensor_shape = output_tensor.sizes();
+  auto expect_shape = std::vector<int64_t>{10, 20, 12, 12};
+  EXPECT_EQ(tensor_shape, expect_shape);
+}
+
+TEST(TestCov2x2, TestOutputShape) {
+  auto conv3x3 = torch::nn::convNxN<2>(3, 20, 1);
+  auto output_tensor = conv3x3(torch::ones({10, 3, 10, 10}));
+  auto tensor_shape = output_tensor.sizes();
+  auto expect_shape = std::vector<int64_t>{10, 20, 11, 11};
   EXPECT_EQ(tensor_shape, expect_shape);
 }
 
---
+++ tests/test_reset.cpp
@@ -0,0 +1,18 @@
+// Copyright ynjassionchen@gmail.com
+#include "../include/residual_block.hpp"
+#include "../include/resnet.hpp"
+
+#include <gtest/gtest.h>
+
+TEST(TestResidualBlock, TestOutputShape) {
+  auto block = torch::nn::ResidualBlock(3, 3, 1);
+  auto output_tensor = block(torch::ones({10, 3, 20, 30}));
+  auto tensor_shape = output_tensor.sizes();
+  auto expect_shape = std::vector<int64_t>{10, 3, 20, 30};
+  EXPECT_EQ(tensor_shape, expect_shape);
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
